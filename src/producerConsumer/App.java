713d67e9e8e166082022b0fb7746567bedcc03f3
xiaotongqiu <xiaotongqiu@tencent.com>
2023-05-21 23:01:26 +0100

    try producer-consumer with BlockingQueue

---
+++ src/producerConsumer/App.java
@@ -0,0 +1,57 @@
+package producerConsumer;
+
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class App {
+    // thread safe, FIFO
+    private final static BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(10);
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            try {
+                producer();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            try {
+                consumer();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+    }
+
+    private static void producer() throws InterruptedException {
+        Random random = new Random();
+        while (true) {
+            // if the queue is full
+            // wait till one item has been removed from the queue
+            queue.put(random.nextInt(100));
+        }
+    }
+
+    private static void consumer() throws InterruptedException {
+        Random random = new Random();
+        while (true) {
+            Thread.sleep(1000);
+
+            if (random.nextInt(10) == 0) {
+                // if there's nothing in the queue,
+                // wait till something comes in
+                Integer value = queue.take();
+                System.out.println("Taken value: " + value + "; Queue size is: " + queue.size());
+            }
+        }
+    }
+}
